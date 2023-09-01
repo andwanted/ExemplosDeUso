0654be14d11c673c10e238cf669fe0d26cc0fd48
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-09-01 15:51:21 -0300

    Programação funcional - Comparator exemplo 1 - comparator - expressao lambda , funcao anonima ou arrow function

--- ws-exemplos/Programacao-Funcional-exemplos-comparator/src/application/Program.java
+++ ws-exemplos/Programacao-Funcional-exemplos-comparator/src/application/Program.java
@@ -19,16 +19,10 @@ public class Program {
 		
 		
 		//1
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product p1, Product p2) {
-				// TODO Auto-generated method stub
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-
+		Comparator<Product> comp = (p1,p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-
+		
 		//2
 		list.sort(comp);
 
