5f253af6cc727135e332c16ef5a842f7da7ce99e
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-09-05 13:16:55 -0300

    Programação Funcional - Exemplo Consumer - implementação da interface

---
+++ ws-exemplos/Programacao-Funcional-exemplos-Consumer/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import service.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Bicicleta", 500.00, 000000, 55533));
+		list.add(new Product("Removedor", 10.00, 20 - 05 - 2023, 55533));
+		list.add(new Product("Feijao - 5kg", 20.00, 10 - 07 - 2023, 55520));
+		list.add(new Product("Notebook", 2000.00, 000000, 55510));
+		list.add(new Product("Camiseta Polo", 80.00, 000000, 55510));
+
+		list.forEach(new PriceUpdate());
+
+		list.forEach(System.out::println);
+
+	}
+
+}
---
+++ ws-exemplos/Programacao-Funcional-exemplos-Consumer/src/entities/Product.java
@@ -0,0 +1,58 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+	private Integer validity;
+	private Integer batch;
+
+	public Product(String name, Double price, Integer validity, Integer batch) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.validity = validity;
+		this.batch = batch;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getValidity() {
+		return validity;
+	}
+
+	public void setValidity(Integer validity) {
+		this.validity = validity;
+	}
+
+	public Integer getBatch() {
+		return batch;
+	}
+
+	public void setBatch(Integer batch) {
+		this.batch = batch;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + ", validity=" + validity + ", batch=" + batch + "]";
+	}
+
+}
