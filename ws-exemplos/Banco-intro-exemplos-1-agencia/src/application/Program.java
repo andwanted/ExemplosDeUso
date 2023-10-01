4f3b0482163b3ba6d80c3defe4ffa40c4d71f778
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-10-01 20:12:07 -0300

    adicionado funcoes consultar adicionar e remover exemplos em banco de dados utilizando jdbc

--- ws-exemplos/Banco-intro-exemplos-1-agencia/src/application/Program.java
+++ ws-exemplos/Banco-intro-exemplos-1-agencia/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,27 +11,25 @@ import entities.Carro;
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void conectar() {
+
+	}
+
+	public static void consultar() {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
 
-		Carro carro = new Carro();
-
 		try {
 			conn = DB.getConnection();
 			st = conn.createStatement();
-			rs = st.executeQuery("select * from veiculos");
+			rs = st.executeQuery("SELECT marca FROM veiculos WHERE marca ='fiat'");
 
 			while (rs.next()) {
+				System.out.println(rs.getString("marca"));
 
-				carro.addCarro(new Carro(rs.getString("marca"), rs.getString("modelo"), rs.getInt("ano"),
-						rs.getString("placa")));
-				System.out.println("Adicionando carro..");
 			}
 
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -38,6 +37,92 @@ public class Program {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
+	}
+
+	public static void inserir() {
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("INSERT INTO veiculos " + "(marca,modelo,ano,placa)" + "VALUES " + "(?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Honda");
+			st.setString(2, "City");
+			st.setString(3, "2011");
+			st.setString(4, "EDE-3417");
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id: " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+	}
+
+	public static void remover() {
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("DELETE FROM veiculos "
+					+ "WHERE ano = ?",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setInt(1, 2018);
+			
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				
+				
+			System.out.println("Done!");
+				
+			} else {
+				System.out.println("No rows affected!");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Carro carro = new Carro();
+
+		//remover();
+		//inserir();
+		consultar();
+		/*
+		 * while (rs.next()) {
+		 * 
+		 * carro.addCarro(new Carro(rs.getString("marca"), rs.getString("modelo"),
+		 * rs.getInt("ano"), rs.getString("placa")));
+		 * System.out.println("Adicionando carro.."); }
+		 * 
+		 */
 
 		for (Carro c : carro.getCarros()) {
 			System.out.println(c.toString());
